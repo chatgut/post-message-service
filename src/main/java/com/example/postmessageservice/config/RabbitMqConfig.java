66130bac7a815dfaa1a9e036b15026bfa6489ff2
Hatice Celik <hatice.celik@iths.se>
2024-05-12 14:41:04 +0200

    Create RabbitMQ exchange, queue and build binding in config.

---
+++ src/main/java/com/example/postmessageservice/config/RabbitMqConfig.java
@@ -0,0 +1,40 @@
+package com.example.postmessageservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue (queue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Binding between queue and exchange with routing key
+    @Bean
+    public Binding binding (){
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with("message_routing_key");
+    }
+}
